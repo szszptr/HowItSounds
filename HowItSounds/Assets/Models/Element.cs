abc939d932f2b93ab34c50a2b7d5032b56511e5e
Peter <Peter@DESKTOP-EKEKSUM>
2016-09-29 19:27:12 +0200

    Add project files.

---
+++ HowItSounds/Assets/Models/Element.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace HowItSounds.Assets.Models
+{
+    class Element
+    {
+        //Class for containing properties.
+        public string Name { get; set; }
+        public string CoverImage { get; set; }
+        public string Sound { get; set; }
+        public int Category { get; set; }
+
+        public Element(string _name, string _image, string _sound, int _category)
+        {
+            Name = _name;
+            CoverImage = _image;
+            Sound = _sound;
+            Category = _category;
+        }
+    }
+    class ElementFactory
+    {
+        //Factory
+        public static List<Element> GetElements(string uriString, string filename)
+        {
+            string _uri = uriString; //Get Config file and elements location
+            string _filename = filename; // Get Config.xml filename
+            string _connect = _uri + filename; // Create connection string
+            List<Element> _elements = new List<Element>(); // New list for the elements
+            try
+            {
+                using (XmlReader reader = XmlReader.Create(_connect)) // Read the Config.xml
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.IsStartElement())
+                        {
+                            string _begin = @"ms-appx:///"; // or could try this.BaseUri
+                            reader.ReadToFollowing("name");
+                            string name = reader.ReadElementContentAsString(); // Car
+                            reader.ReadToFollowing("image");
+                            string image = _begin + _uri + reader.ReadElementContentAsString(); // ms-appx:///Assets/Elements/car.jpg
+                            reader.ReadToFollowing("sound");
+                            string sound = _begin + _uri + reader.ReadElementContentAsString(); // ms-appx:///Assets/Elements/car.wav
+                            reader.ReadToFollowing("category");
+                            int category = reader.ReadElementContentAsInt(); // 1
+                            _elements.Add(new Element(name, image, sound, category));
+                        }
+                    }
+                }
+            }
+            catch (System.IO.FileNotFoundException) { return _elements; }
+            return _elements;
+        }
+    }
+}
---
+++ HowItSounds/MainPage.xaml.cs
@@ -0,0 +1,145 @@
+﻿using HowItSounds.Assets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Xaml.Input;
+
+namespace HowItSounds
+{
+    /// <summary>
+    /// Let's play some sounds!
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<Element> ElementsList;
+        // App configuration, default error messages.
+        private string configurationErrorMessage = "Configuration error!";
+        private string fileErrorMessage = "File or configuration error!";
+        // App configuration, where are the sound files, the pictures, and the Config.xml file.
+        private string AssetElements = @"Assets/Elements/";
+        private string configFile = @"Config.xml";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Application.Current.Resuming += new EventHandler<Object>(App_Resuming);
+            Windows.ApplicationModel.DataTransfer.DataTransferManager.GetForCurrentView().DataRequested += MainPage_DataRequested;
+        }
+
+        private void App_Resuming(object sender, object e)
+        {
+            FillList();
+        }
+
+        private void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+           FillList();
+        }
+
+        private void FillList()
+        {
+            
+            //Populate a list with the given configuration.
+            ElementsList = new List<Element>(ElementFactory.GetElements(AssetElements, configFile));
+
+            if (ElementsList.Count == 0)
+            {
+                SetTitleTextBlock(configurationErrorMessage);
+            }
+            else
+            {
+                //MyGridView's source is the populated list.
+                MyGridView.ItemsSource = ElementsList;
+            }
+        }
+
+        private async void MyGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            //Get the clicked item and set the uri to the sound file.
+            var _whatToPlay = (Element)e.ClickedItem;
+            var _uri = new Uri(_whatToPlay.Sound.ToString());
+
+            // Try to set the mediaelement's source to the soundfile. Gives error, if the file is not there, or the Config.xml contains wrong information (misspelled name).
+            try
+            {
+                var _file = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(_uri);
+                var _stream = await _file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                idMedia.SetSource(_stream, _file.ContentType);
+            }
+            catch (System.IO.FileNotFoundException) { SetTitleTextBlock(fileErrorMessage); }
+        }
+
+        private void SetTitleTextBlock(string text)
+        {
+            TopTitleTextBlock.Text = text;
+            BottomTitleTextBlock.Text = text;
+        }
+
+        private void AppBarButtonAll_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            MyGridView.ItemsSource = ElementsList;
+        }
+
+        private void AppBarPointer_Entered (object sender, PointerRoutedEventArgs e)
+        {
+            EnterStoryboard.Begin();
+        }
+
+        private void AppBarPointer_Exited(object sender, PointerRoutedEventArgs e)
+        {
+            ExitStoryboard.Begin();
+        }
+
+        private void MainPage_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            args.Request.Data.SetText(TopAppBarButtonShare.Tag.ToString());
+            args.Request.Data.Properties.Title = TopTitleTextBlock.Text;
+        }
+
+        private void AppBarButtonShare_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Windows.ApplicationModel.DataTransfer.DataTransferManager.ShowShareUI();
+        }
+
+        private void MenuFlyoutItem_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            MenuFlyoutItem menuFlyoutItem = sender as MenuFlyoutItem;
+            //Getting which category to look up from MenuFlyoutItem Tag properties (e.g. 1)
+            var _lookUp = Convert.ToInt16(menuFlyoutItem.Tag);
+            //Using the number as category reference after conversion (e.g. 1)
+            MyGridView.ItemsSource = ElementsList.Where(x => x.Category == _lookUp);
+        }
+
+        private void MyGridView_SizeChanged(object sender, Windows.UI.Xaml.SizeChangedEventArgs e)
+        {
+            var _panel = (ItemsWrapGrid)MyGridView.ItemsPanelRoot;
+
+            var _actual = VisualStateGroup.CurrentState;
+            int _gridColumnNumber;
+            switch (_actual.Name)
+            {
+                case "medium":
+                    {
+                        _gridColumnNumber = 2;
+                        break;
+                    }
+                case "large":
+                    {
+                        _gridColumnNumber = 3;
+                        break;
+                    }
+                default:
+                    {
+                        _gridColumnNumber = 1;
+                        break;
+                    }
+            }
+
+            ////panel.ItemWidth = panel.ItemHeight = e.NewSize.Width / _gridColumnNumber;
+            _panel.ItemWidth = e.NewSize.Width / _gridColumnNumber;
+        }
+    }
+}
