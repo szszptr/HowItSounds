69cf6b64b88229f08a6b9d4348ecb88d4db52c40
Peter <szszptr@gmail.com>
2016-10-06 23:08:44 +0200

    New category and contents. Logo changes.

--- HowItSounds/Properties/AssemblyInfo.cs
+++ HowItSounds/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HowItSounds")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("UWP App")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Péter Szász")]
 [assembly: AssemblyProduct("HowItSounds")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Péter Szász")]
 [assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
